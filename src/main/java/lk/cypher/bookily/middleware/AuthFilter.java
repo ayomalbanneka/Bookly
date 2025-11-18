e5016f23c698c73416de9d87a9778144605de974
Ayomal Banneka <166504527+ayomalbanneka@users.noreply.github.com>
2025-11-18 21:13:08 +0530

    Add AuthFilter to manage user authentication and redirect logic

---
+++ src/main/java/lk/cypher/bookily/middleware/AuthFilter.java
@@ -0,0 +1,34 @@
+package lk.cypher.bookily.middleware;
+
+import jakarta.annotation.Priority;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.ws.rs.Priorities;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+import lk.cypher.bookily.annotation.IsUser;
+
+import java.io.IOException;
+import java.net.URI;
+
+@Provider
+@IsUser
+@Priority(Priorities.AUTHENTICATION)
+public class AuthFilter implements ContainerRequestFilter {
+
+    @Context
+    private HttpServletRequest request;
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
+        HttpSession httpSession = request.getSession(false);
+        if (httpSession != null && httpSession.getAttribute("user") != null) {
+            containerRequestContext
+                    .abortWith(Response.status(Response.Status.TEMPORARY_REDIRECT)
+                            .location(URI.create(request.getContextPath() + "/index.html")).build());
+        }
+    }
+}
