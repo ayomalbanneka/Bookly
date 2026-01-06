addfd35561ff060b6f5639ee1f5e39535b0e0e39
Ayomal Banneka <166504527+ayomalbanneka@users.noreply.github.com>
2026-01-06 18:48:08 +0530

    Add PayHereDTO for payment data transfer
    Introduces PayHereDTO as a Serializable class to encapsulate payment-related fields for integration with PayHere. This DTO includes merchant, order, customer, and transaction details, supporting both live and sandbox environments.

---
+++ src/main/java/lk/cypher/bookliy/dto/PayHereDTO.java
@@ -0,0 +1,159 @@
+package lk.cypher.bookliy.dto;
+
+import java.io.Serializable;
+
+public class PayHereDTO implements Serializable {
+    private String merchant_id;
+    private String return_url;
+    private String cancel_url;
+    private String notify_url;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private String country;
+    private String order_id;
+    private String items;
+    private String currency;
+    private String amount;
+    private String hash;
+    private boolean sandbox;
+
+    public String getMerchant_id() {
+        return merchant_id;
+    }
+
+    public void setMerchant_id(String merchant_id) {
+        this.merchant_id = merchant_id;
+    }
+
+    public String getReturn_url() {
+        return return_url;
+    }
+
+    public void setReturn_url(String return_url) {
+        this.return_url = return_url;
+    }
+
+    public String getCancel_url() {
+        return cancel_url;
+    }
+
+    public void setCancel_url(String cancel_url) {
+        this.cancel_url = cancel_url;
+    }
+
+    public String getNotify_url() {
+        return notify_url;
+    }
+
+    public void setNotify_url(String notify_url) {
+        this.notify_url = notify_url;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
+    }
+
+    public String getItems() {
+        return items;
+    }
+
+    public void setItems(String items) {
+        this.items = items;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public boolean isSandbox() {
+        return sandbox;
+    }
+
+    public void setSandbox(boolean sandbox) {
+        this.sandbox = sandbox;
+    }
+}
