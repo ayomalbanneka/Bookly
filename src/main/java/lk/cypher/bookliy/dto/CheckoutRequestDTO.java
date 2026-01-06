c30c2a6abc3ea88b8b0615d26da37d7bca4cb2f7
Ayomal Banneka <166504527+ayomalbanneka@users.noreply.github.com>
2026-01-06 18:48:03 +0530

    Add CheckoutRequestDTO for checkout data transfer
    Introduces CheckoutRequestDTO to encapsulate checkout-related address and contact information, including fields for current address, name, city, address lines, postal code, and mobile number.

---
+++ src/main/java/lk/cypher/bookliy/dto/CheckoutRequestDTO.java
@@ -0,0 +1,76 @@
+package lk.cypher.bookliy.dto;
+
+public class CheckoutRequestDTO {
+    private boolean isCurrentAddress;
+    private String firstName;
+    private String lastName;
+    private int cityId;
+    private String lineOne;
+    private String lineTwo;
+    private String postalCode;
+    private String mobile;
+
+    public boolean isCurrentAddress() {
+        return isCurrentAddress;
+    }
+
+    public void setCurrentAddress(boolean currentAddress) {
+        isCurrentAddress = currentAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getLineOne() {
+        return lineOne;
+    }
+
+    public void setLineOne(String lineOne) {
+        this.lineOne = lineOne;
+    }
+
+    public String getLineTwo() {
+        return lineTwo;
+    }
+
+    public void setLineTwo(String lineTwo) {
+        this.lineTwo = lineTwo;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
