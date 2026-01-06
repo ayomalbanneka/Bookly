275eb46e91007404a4e1c48edc462bc180108d17
Ayomal Banneka <166504527+ayomalbanneka@users.noreply.github.com>
2026-01-06 18:49:17 +0530

    Add NON_DIGIT_PATTERN constant to Validator
    Introduced NON_DIGIT_PATTERN regex constant to Validator for matching non-digit characters. This can be used for input validation where non-digit detection is required.

--- src/main/java/lk/cypher/bookliy/validation/Validator.java
+++ src/main/java/lk/cypher/bookliy/validation/Validator.java
@@ -5,6 +5,7 @@ public class Validator {
     public static final String PASSWORD_VALIDATION = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$";
     public static final String MOBILE_VALIDATION = "^(0{1})(7{1})([0|1|2|4|5|6|7|8]{1})([0-9]{7})";
     public static final String IS_INTEGER = "^[0-9]+$";
+    public static final String NON_DIGIT_PATTERN = "\\D+";
 
     public static final String VERIFICATION_CODE_VALIDATION = "\\d{6}";
 }
